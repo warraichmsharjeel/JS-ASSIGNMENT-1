c94f23836cb05663b58646045c9341b2180b4a13
SHARJEEL <warraichmsharjeel7@gmail.com>
2026-07-17 11:48:30 +0500

    create js-assignment-1

---
+++ index.js
@@ -0,0 +1,170 @@
+//------CHAPTER NO : 01------//
+//------Question no :01------//
+
+alert("Hello! Broken Heart")
+
+//------Question no :02------//
+
+alert("Error! Please enter a valid password")
+
+//------Question no :03------//
+
+alert("Welcome to JS Land.... \n  Happy coding!")
+
+//------Question no :04------//
+
+alert(" Happy coding! \n  prevent this page from creating additionally dialogs")
+
+//------Question no :05------//
+
+alert("Hello.. I can run JS through my web browser's code ")
+
+//------CHAPTER NO : 02------//
+//------Question no :01------//
+
+ var username = ""
+
+//------Question no :02------//
+
+var myName = "sharjeel"
+
+//------Question no :03------//
+
+var message = "Hello World!"
+
+//------Question no :04------//
+
+var name_age_course = "sharjeel \n\n 15 year's \n\n Web Development Course by \n\n Muhammad Asif"
+alert(name_age_course )
+
+//------Question no :05------//
+
+var Fname ="sharjeel \n sharjee \n Sharje \n Sharj \n Shar \n Sha \n Sh \n S"
+alert(Fname)
+
+//------Question no :06------//
+
+var email ="My email address is sharjeel@gmail.com"
+alert(email)
+
+//------Question no :07------//
+
+var book ="Book name (shiddi)"
+alert(book)
+
+//------Question no :08------//
+
+document.write("Yeah! I can write HTML through Java Script")//
+
+//------CHAPTER NO : 03------//
+//------Question no :01------//
+
+var age ="I am 16 year's old"
+alert(age)
+
+//------Question no :02------//
+
+alert("You have visited this website about 999 times")
+
+//------Question no :03------//
+
+document.write("My birth year is 2010")
+
+//------Question no :04------//
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+//------CHAPTER NO : 05------//
+//------Question no :01------//
+
+var a = 3;
+var b = 5;
+alert(a+b)
+//------Question no :02------//
+
+var a = 3;
+var b = 5;
+alert(a - b)
+alert(a * b)
+alert(a / b)
+alert(a % b)
+
+//------Question no :03------//
+
+var a = 3;
+var b = 5;
+alert(a - b)
+alert(a * b)
+alert(a / b)
+alert(a % b)
+  
+
+//------Question no :04------//
+
+var ticket_price=600;
+var Price_of_5_tickets = 5*600;
+alert(Price_of_5_tickets)
+
+//------Question no :05------//
+
+var a = 4*1
+var b = 4*2
+var c = 4*3
+var d = 4*4
+var e = 4*5
+var f = 4*2
+var g = 4*6
+var h = 4*7
+var i = 4*8
+var j = 4*9
+var k = 4*10
+alert("Table of 4"+ 
+"4 x 1 = " + a + "\n" +
+"4 x 2 = " + b + "\n" +
+"4 x 3 = " + c + "\n" +
+"4 x 4 = " + d + "\n" +
+"4 x 5 = " + e + "\n" +
+"4 x 6 = " + f + "\n" +
+"4 x 7 = " + g + "\n" +
+"4 x 8 = " + h + "\n" +
+"4 x 9 = " + i + "\n" +
+"4 x 10 = " + j
+)
+
+//------Question no :06------//
+
+
+//------Question no :07------//
+
+var a = "Price of item 1 is  =" ; 
+var b = 250 ;
+var c = "Quantity of item 1 is=  ";
+var d = 3 ;
+var e = "Price of item 2 is= " ;
+var f = 100;
+var g = "Quantity of item 2 is = ";
+var h = 7;
+var i =  "Shipping charges =";
+var j = 100;
+var k =250*3 +100*7 +100
+alert( a + b + "\n"+c + d + "\n" + e + f + "\n" + g + h + "\n"+ i + j + "\n"+"Total cost of your order is = " +k)
+
+
+//------Question no :08------//
+
+var total_marks = 980 ;
+var obtained_marks = 804 ;
+var Percentage = 82.04 ;
+alert("Total Marks ="+total_marks+"\n"+ "Obtained marks =" +obtained_marks +"\n"+"Percentage ="+Percentage +"%" )
+
+//------Question no :09------//
+
+var Tota_$ = 10;
+var $_in_PKR = 104.08*10
+var Total_SR = 25
+var SR_in_PKR = 25*28
+var Total_PKR = $_in_PKR+SR_in_PKR
+alert( "Toatal PKR = "+Total_PKR)
